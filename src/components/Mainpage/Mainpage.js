e81a546b80e638447184211808e509624eefead5
Bulat Nugmanov <bbnugmanov@gmail.com>
2018-12-05 15:42:07 -0800

    add location fetching to click handler

--- src/components/Mainpage/Mainpage.js
+++ src/components/Mainpage/Mainpage.js
@@ -7,44 +7,80 @@ import { fetchRecommendations} from "../../actions";
 import './Mainpage.css';
 import {connect} from "react-redux";
 
-type Props = {
-  fetchRecommendations: () => void
+type State = {
+  errorMessage: string
 }
 
-class Main extends Component<Props> {
+type Props = {
+  fetchRecommendations: () => void,
+}
+
+class Main extends Component<Props, State> {
+
+  state = {
+    errorMessage: undefined
+  };
+
+  handleLocationError = () => {
+    this.setState({
+      errorMessage: "Something went wrong getting your location :("
+    });
+  };
+
+  handleLocationSuccess = (position: Position) => {
+
+    this.props.fetchRecommendations({
+      location: {
+        lat: position.coords.latitude,
+        lon: position.coords.longitude
+      }
+    });
+
+    this.props.history.push('/map');
+
+  };
 
   onContinue = () => {
-    this.props.fetchRecommendations({}); // todo
-    this.props.history.push('/map');
+
+    if(!("geolocation" in navigator)) {
+      this.setState({
+        errorMessage: "Need to know your location for this to work :)"
+      });
+      return;
+    }
+
+    navigator.geolocation.getCurrentPosition(
+      this.handleLocationSuccess,
+      this.handleLocationError
+    );
+
   };
 
   render() {
 
     return (
       <div className="App">
-        <header className="App-header">
-          <p>Logo</p>
-					<div>
-						<FilledInput 
-							className="input"
-						/>
-						<Button
-              color="primary"
-              onClick={this.onContinue}
-							variant="contained"
-							className="search">
-							GO
-						</Button>
-						</div>
-          <div
-            style={{padding: 16}}
-          />
-        </header>
+        <p>Logo</p>
+        <div>
+          <FilledInput className="input"/>
+          <Button
+            color="primary"
+            onClick={this.onContinue}
+            variant="contained"
+            style={{ marginLeft: 20 }}
+            className="search">
+            GO
+          </Button>
+        </div>
+        {
+          this.state.errorMessage &&
+          <div>this.state.errorMessage</div>
+        }
       </div>
     );
   }
 }
 
 export default connect(null, {
-	fetchRecommendations
+  fetchRecommendations
 })(Main);
