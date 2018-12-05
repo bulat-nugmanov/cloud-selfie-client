f5f75e77bbc6d29bddd19c6840519c568a8d1695
Bulat Nugmanov <bbnugmanov@gmail.com>
2018-12-05 14:02:52 -0800

    connect react with redux

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Provider } from "react-redux";
+import store from "./store";
 
-class App extends Component {
+class Main extends Component {
 
   render() {
     return (
@@ -24,6 +26,16 @@ class App extends Component {
       </div>
     );
   }
+}
+
+class App extends Component {
+
+  render = () => (
+    <Provider store={store}>
+      <Main/>
+    </Provider>
+  )
+
 }
 
 export default App;
