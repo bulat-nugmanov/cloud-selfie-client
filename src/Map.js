2533dbec80478a12ae60ae7bc671f93c9accc474
Bulat Nugmanov <bbnugmanov@gmail.com>
2018-12-05 17:11:51 -0800

    added map

--- src/Map.js
+++ src/Map.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import type { Recommendation} from "./types";
+import ReactMapGL from 'react-map-gl';
 import {connect} from "react-redux";
 
 type Props = {
@@ -13,13 +14,29 @@ export class Map extends React.Component<Props> {
     fetching: false
   };
 
+  state = {
+    viewport: {
+      width: Math.max(document.documentElement.clientWidth, window.innerWidth || 0),
+      height: Math.max(document.documentElement.clientHeight, window.innerHeight || 0),
+      latitude: 49.2827,
+      longitude: -123.1207,
+      zoom: 8
+    }
+  };
+
   render = () => {
 
     // todo:
     // if this.props.fetching show loading spinner
 
     return (
-      <div>todo: show recommendations</div>
+      <div>
+        <ReactMapGL
+          mapboxApiAccessToken={"pk.eyJ1IjoibnVnbWFub3YiLCJhIjoiY2pwYndkbG5rMjViNTNrcXNhamhrNHVjMyJ9.-tmLxyTB-6_2teDQpYcFQg"}
+          {...this.state.viewport}
+          onViewportChange={(viewport) => this.setState({viewport})}
+        />
+      </div>
     )
   }
 
